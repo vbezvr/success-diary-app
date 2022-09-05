8e3eff10a8b0f099b637fe4fb608d301323000fa
Viktoria <vebzvrh@gmail.com>
2022-09-05 15:28:37 +0300

    habit tracker

--- src/components/HabitCard.js
+++ src/components/HabitCard.js
@@ -1,31 +1,73 @@
-import { format } from "date-fns";
+import { eachDayOfInterval, format, subDays } from "date-fns";
 import { onValue, ref, set } from "firebase/database";
 import { useContext, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { context } from "..";
-import plus from "../img/plus.svg"
+import { toCapitalize } from "../helper";
+import plus from "../img/plus.svg";
+
+function DayItem({data}) {
+  const day = format(data, "d");
+  const weekDay = format(data, "EEE");
+  console.log(weekDay)
+  return (
+    <div className="item">
+      <p className="days">{weekDay}</p>
+      <p className="number">{day}</p>
+      <hr></hr>
+    </div>
+  );
+}
+
+function HabitItem({data, days}) {
+  const habitName = toCapitalize(data.name)
+  const daysItem = days.map((day, index) => <DayItem key={index} data={day} />)
+  return (<div className="habit-item">
+    <span>{habitName}</span>
+    <div className="items-wrapper">
+      {daysItem}
+    </div>
+  </div>)
+}
 
 function HabitCard() {
-  const {db} = useContext(context);
-  const {uid} = useSelector((state) => state.userConfig);
-  const [habits, setHabits] = useState([])
+  const { db } = useContext(context);
+  const { uid } = useSelector((state) => state.userConfig);
+  const [habits, setHabits] = useState([]);
   const habitsRef = ref(db, `habits/${uid}`);
-  // const currentData = format(new Date(), )
+  const currentDay = format(new Date(), "yyyy-MM-dd");
+  const dayWeekBefore = subDays(new Date(currentDay), 6);
+  const daysOfWeek = eachDayOfInterval({
+    start: dayWeekBefore,
+    end: new Date(currentDay),
+  });
 
   useEffect(() => {
-    onValue(habitsRef, (snap) => {
-      const snapSize = Object.values(snap.val()).length;
-      if (habits.length !== snapSize) {
-        setHabits([])
-        snap.forEach((habitData) => {
-            setHabits((prevState) => [...prevState, habitData.key])
-        })
-      }
-    }, 
-    {onlyOnce: true})
-  })
-  console.log(habits)
-  
+    onValue(
+      habitsRef,
+      (snap) => {
+        const snapSize = Object.values(snap.val()).length;
+        if (habits.length !== snapSize) {
+          setHabits([]);
+          snap.forEach((habitData) => {
+            setHabits((prevState) => [
+              ...prevState,
+              { name: habitData.key, dates: habitData.val() },
+            ]);
+          });
+        }
+      },
+      { onlyOnce: true }
+    );
+  }
+  );
+  // console.log(daysOfWeek)
+  function createHabitsItems() {
+    return habits.map((elem, index) => (
+      <HabitItem data={elem} key={index} days={daysOfWeek} />
+    ));
+  }
+
   return (
     <div className="card">
       <div className="habits">
@@ -38,9 +80,12 @@ function HabitCard() {
             </div>
           </div>
         </div>
+        <div className="habits-form">
+          {createHabitsItems()}
+        </div>
       </div>
     </div>
   );
 }
 
-export {HabitCard}
+export { HabitCard };
---
+++ src/helper.js
@@ -0,0 +1,5 @@
+function toCapitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export { toCapitalize };
