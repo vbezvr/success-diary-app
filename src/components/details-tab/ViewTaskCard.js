8f696b3912c1b16ab3a08a6b08a92a9000011a05
Viktoria <vebzvrh@gmail.com>
2022-10-09 19:50:18 +0300

    view task description

--- src/components/details-tab/ViewTaskCard.js
+++ src/components/details-tab/ViewTaskCard.js
@@ -9,6 +9,7 @@ import { context } from "../..";
 function ViewTaskCard() {
   const [date, setDate] = useState(new Date());
   const [tasks, setTasks] = useState([]);
+  const [currentTask, setCurrentTask] = useState(0);
   const formatData = format(date, "yy-MM-dd");
   const { db } = useContext(context);
   const { uid } = useSelector((state) => state.userConfig);
@@ -32,9 +33,12 @@ function ViewTaskCard() {
 
   console.log(tasks);
   const viewItems = tasks.map((elem, index) => (
-    <TaskItem key={index} data={elem} index={index} />
+    <TaskItem key={index} data={elem} index={index} handleClick={handleClick} />
   ));
 
+  function handleClick(index) {
+    setCurrentTask(index);
+  }
   function handleSubmit(event) {
     event.preventDefault();
   }
@@ -54,41 +58,47 @@ function ViewTaskCard() {
             </div>
           </form>
           <div className="habits-items-wrapper">{viewItems}</div>
-          <div className="view-task-card">
-            <div className="view-wrapper">
-              <div className="title">
-                <h3>Title:</h3>
-              </div>
-              <div className="content"></div>
-            </div>
-            <div className="view-wrapper">
-              <div className="title">
-                <h3>Category:</h3>
-              </div>
-              <div className="content"></div>
-            </div>
-            <div className="view-wrapper">
-              <div className="title">
-                <h3>Description:</h3>
-              </div>
-              <div className="content"></div>
-            </div>
-          </div>
+          <TaskDescriptions data={tasks[currentTask]} />
         </div>
       </div>
     </div>
   );
 }
 
-function TaskItem({data, index}) {
-  
+function TaskItem({ data, index, handleClick }) {
   return (
-    <div className="view-item-wrapper habits-view">
+    <div
+      className="view-item-wrapper habits-view"
+      onClick={() => handleClick(index)}
+    >
       <div className="view-item">
         <div className="title title-habit">{data.title}</div>
-        {/* <div className="img-delete" onClick={() => handleDeleteField(name)}>
-          <img width="25px" src={remove_icon} />
-        </div> */}
+        <div className="category-name">{data.activeCategory}</div>
+      </div>
+    </div>
+  );
+}
+
+function TaskDescriptions({ data }) {
+  return (
+    <div className="view-task-card">
+      <div className="view-wrapper">
+        <div className="title">
+          <h3>Title:</h3>
+        </div>
+        <div className="content">{data.title}</div>
+      </div>
+      <div className="view-wrapper">
+        <div className="title">
+          <h3>Category:</h3>
+        </div>
+        <div className="content">{data.activeCategory}</div>
+      </div>
+      <div className="view-wrapper">
+        <div className="title">
+          <h3>Description:</h3>
+        </div>
+        <div className="content">{data.value}</div>
       </div>
     </div>
   );
