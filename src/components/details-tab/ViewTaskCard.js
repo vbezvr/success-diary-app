deb1e5605ffef44a061b83b145e59ea0631919ed
Viktoria <vebzvrh@gmail.com>
2022-10-09 19:25:42 +0300

    0910

--- src/components/details-tab/ViewTaskCard.js
+++ src/components/details-tab/ViewTaskCard.js
@@ -1,5 +1,5 @@
 import { format } from "date-fns";
-import { get, ref } from "firebase/database";
+import { onValue, ref } from "firebase/database";
 import { useContext, useEffect, useState } from "react";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -8,35 +8,32 @@ import { context } from "../..";
 
 function ViewTaskCard() {
   const [date, setDate] = useState(new Date());
-  const [tasks, setTasks] = useState({ forDate: null, data:[] });
+  const [tasks, setTasks] = useState([]);
   const formatData = format(date, "yy-MM-dd");
   const { db } = useContext(context);
   const { uid } = useSelector((state) => state.userConfig);
 
   useEffect(() => {
     const dateRef = ref(db, `users/${uid}/${formatData}`);
-    get(dateRef).then((snap) => {
-      if (snap.exists() && tasks.forDate !== formatData) {
-        setTasks((prevState) => Object.assign(prevState, {forDate: formatData}));
-        console.log("work")
-      }
-      return snap;
-    }).then((snap) => {
-      if (snap && tasks) {
-        console.log(tasks)
-        snap.forEach((elem) => {
-          setTasks((prevState) => {
-            Object.assign({}, prevState, {
-              data: [ elem.val()],
-            });
+    onValue(
+      dateRef,
+      (snap) => {
+        if (snap.exists()) {
+          setTasks([]);
+          snap.forEach((snapChild) => {
+            setTasks((prevState) => [...prevState, snapChild.val()]);
           });
-        });
+        }
+      },
 
-      }
-    });
-  });
+      { onlyOnce: true }
+    );
+  }, [date]);
 
-  console.log(tasks)
+  console.log(tasks);
+  const viewItems = tasks.map((elem, index) => (
+    <TaskItem key={index} data={elem} index={index} />
+  ));
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -56,7 +53,7 @@ function ViewTaskCard() {
               <input type="submit" className="submit details" value="Show" />
             </div>
           </form>
-          <div className="habits-items-wrapper">{}</div>
+          <div className="habits-items-wrapper">{viewItems}</div>
           <div className="view-task-card">
             <div className="view-wrapper">
               <div className="title">
@@ -78,6 +75,20 @@ function ViewTaskCard() {
             </div>
           </div>
         </div>
+      </div>
+    </div>
+  );
+}
+
+function TaskItem({data, index}) {
+  
+  return (
+    <div className="view-item-wrapper habits-view">
+      <div className="view-item">
+        <div className="title title-habit">{data.title}</div>
+        {/* <div className="img-delete" onClick={() => handleDeleteField(name)}>
+          <img width="25px" src={remove_icon} />
+        </div> */}
       </div>
     </div>
   );
